3dd57894a9ee92aaa9407fc186b56fedd6543c51
uae <aniekaudo@yahoo.com>
2022-04-29 15:35:09 +0100

    :card_file_box: Database
    Creating model for users, comments, likes and posts

--- db/migrate/20220428214757_add_user_ref_to_likes.rb
+++ db/migrate/20220428214757_add_user_ref_to_likes.rb
@@ -1,7 +1,6 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, null: false, foreign_key: true
-    add_column :likes, :user, :string
-    add_index :likes, :user
+    add_reference :likes, :author, null: false, foreign_key: {to_table: :users}
+    
   end
 end
