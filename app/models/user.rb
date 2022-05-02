549dbfe905b8996ada6fb5c4ee53115cedb4180e
uae <aniekaudo@yahoo.com>
2022-05-02 19:30:10 +0100

    :card_file_box: User
    Created user in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
 end
