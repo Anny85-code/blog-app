7b947745a4340fdd3ade9b4933b8803b8bb58574
uae <aniekaudo@yahoo.com>
2022-05-02 22:21:54 +0100

    :card_file_box: Model
    Created posts and comments in Comment and post models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Bassey!' )
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Samuel!' )
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Elijah!' )
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Helen!' )
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Eunice!' )
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  
+  first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+  
+  first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+  
+  first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+  
+  first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
+  
+  
 end
