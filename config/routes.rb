6b8d2aae21af166d8f50b1e70fb24e7ef694821a
uae <aniekaudo@yahoo.com>
2022-05-05 11:22:37 +0100

    :card_file_box: Views
    Render all posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   # get "users/:user_id/posts/:id", to: "posts#show"
 
   resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show]
+      resources :posts, only: [:index, :show] do
+        resources :comments, only: [:index, :show]
+        resources :likes, only: [:index, :show]
+      end
   end
   # Defines the root path route ("/")
   root "users#index"
