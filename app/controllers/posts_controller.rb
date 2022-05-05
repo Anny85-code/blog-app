91225bcd593994aabd457bd5dfbb14b453f5be1b
uae <aniekaudo@yahoo.com>
2022-05-05 23:56:21 +0100

    :sparkles: Forms
    Added methods for creating new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+  end
 end
