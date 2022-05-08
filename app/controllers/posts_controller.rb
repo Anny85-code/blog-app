ee65b6a5c92ac0f3183a36f5a71dddda67d6dbbc
uae <aniekaudo@yahoo.com>
2022-05-08 08:55:54 +0100

    :coffin: Clean codebase
    Removed dead codes from codebase

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @user = User.includes(:posts, :comments, :likes).find(params[:user_id])
     @posts = Post.all
-    # @user = User.includes(:posts).find(params[:user_id])
   end
 
   def show
