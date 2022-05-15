a1972495168be9635da73348f2c6f4efe89e13e5
uae <aniekaudo@yahoo.com>
2022-05-15 10:11:37 +0100

    :alien: API
    Setting strctures for api generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
 
   before_action :authenticate_user!
